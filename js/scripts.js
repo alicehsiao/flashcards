11a96dff97c1a8f8253d516276aa5123b53b5d3d
Alice Hsiao <alice.hsiao87@gmail.com>
2018-02-22 18:01:11 -0800

    add initial css, js, and html files

---
+++ js/scripts.js
@@ -0,0 +1,78 @@
+$(document).ready(function(){
+  $("#clickable").click(function() {
+    $("#hidden").toggle();
+    $("#showing").toggle();
+  });
+  $("#clickable2").click(function() {
+    $("#hidden2").toggle();
+    $("#showing2").toggle();
+  });
+  $("#clickable3").click(function() {
+    $("#hidden3").toggle();
+    $("#showing3").toggle();
+  });
+  $("#clickable4").click(function() {
+    $("#hidden4").toggle();
+    $("#showing4").toggle();
+  });
+  $("#clickable5").click(function() {
+    $("#hidden5").toggle();
+    $("#showing5").toggle();
+  });
+  $("#clickable6").click(function() {
+    $("#hidden6").toggle();
+    $("#showing6").toggle();
+  });
+  $("#clickable7").click(function() {
+    $("#hidden7").toggle();
+    $("#showing7").toggle();
+  });
+  $("#clickable8").click(function() {
+    $("#hidden8").toggle();
+    $("#showing8").toggle();
+  });
+  $("#clickable9").click(function() {
+    $("#hidden9").toggle();
+    $("#showing9").toggle();
+  });
+  $("#clickable10").click(function() {
+    $("#hidden10").toggle();
+    $("#showing10").toggle();
+  });
+  $("#clickable11").click(function() {
+    $("#hidden11").toggle();
+    $("#showing11").toggle();
+  });
+  $("#clickable12").click(function() {
+    $("#hidden12").toggle();
+    $("#showing12").toggle();
+  });
+  $("#clickable13").click(function() {
+    $("#hidden13").toggle();
+    $("#showing13").toggle();
+  });
+  $("#clickable14").click(function() {
+    $("#hidden14").toggle();
+    $("#showing14").toggle();
+  });
+  $("#clickable15").click(function() {
+    $("#hidden15").toggle();
+    $("#showing15").toggle();
+  });
+  $("#clickable16").click(function() {
+    $("#hidden16").toggle();
+    $("#showing16").toggle();
+  });
+  $("#clickable17").click(function() {
+    $("#hidden17").toggle();
+    $("#showing17").toggle();
+  });
+  $("#clickable18").click(function() {
+    $("#hidden18").toggle();
+    $("#showing18").toggle();
+  });
+  $("#clickable19").click(function() {
+    $("#hidden19").toggle();
+    $("#showing19").toggle();
+  });
+});
